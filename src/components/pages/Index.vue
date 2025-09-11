19c0f2bd8849858b6ac9a1d74258d29597a41e93
odaialzoubi <odaialzoubi2011@gmail.com>
2025-09-11 20:31:36 +0300

    Create Index page in Pages as main entry for index view

---
+++ src/components/pages/Index.vue
@@ -0,0 +1,21 @@
+<template>
+    <Master>
+        <template #content>
+            <!-- <Alert :successMessage="successMessage" :errorMessage="errorMessage" /> -->
+            <div class="content d-flex flex-column flex-column-fluid">
+                <div class="post d-flex flex-column-fluid">
+                    <div class="container-xxl" style="max-width: none;">
+                        <div class="card card-flush">
+                            <slot name="content" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <slot name="modal" />
+        </template>
+    </Master>
+</template>
+
+<script setup>
+import Master from '@/components/MainContent'
+</script>
--- src/views/students/Index.vue
+++ src/views/students/Index.vue
@@ -1,153 +1,143 @@
 <template>
-    <Master>
+    <Index>
+        <!-- Index Page -->
         <template #content>
-            <Alert :successMessage="successMessage" :errorMessage="errorMessage" />
-            <div class="content d-flex flex-column flex-column-fluid">
-                <div class="post d-flex flex-column-fluid">
-                    <div class="container-xxl" style="max-width: none;">
-                        <div class="card card-flush">
-                            <div class="card-header align-items-center py-5 gap-2 gap-md-5">
-                                <!--begin::Card title-->
-                                <div class="card-title">
-                                </div>
-                                <!--begin::Card toolbar-->
-                                <div class="card-toolbar flex-row-fluid justify-content-end gap-5">
-                                    <!--begin::Add product-->
-                                    <button @click="openAddModal" class="btn btn-primary">{{ t('table.add') }} {{ t('student.student') }}</button>
-                                    <router-link v-if="route.name == 'student'" :to="{ name: 'student.onlyTrashed' }"
-                                        class="btn btn-primary">{{ t('student.students') }} {{ t('student.trashed') }}</router-link>
-                                    <router-link v-if="route.name == 'student.onlyTrashed'" :to="{ name: 'student' }"
-                                        class="btn btn-primary">{{ t('student.students') }} Active</router-link>
-                                    <Filter @submit="fetchStudents" />
-                                </div>
-                                <!--end::Card toolbar-->
-                            </div>
-                            <!-- Table -->
-                            <div class="card-body pt-0 table-responsive">
-                                <table class="table align-middle table-row-dashed fs-6 gy-5">
-                                    <thead>
-                                        <tr class="text-start text-gray-400 fw-bold fs-7 text-uppercase gs-0">
-                                            <th class="w-10px pe-2">
-                                                <div
-                                                    class="form-check form-check-sm form-check-custom form-check-solid me-3">
-                                                    <input class="form-check-input" type="checkbox" data-kt-check="true"
-                                                        data-kt-check-target="#kt_ecommerce_products_table .form-check-input"
-                                                        value="1" />
-                                                </div>
-                                            </th>
-                                            <th class="min-w-100px">{{ t('form.id') }}</th>
-                                            <!-- <th v-for="n in thead" class=" min-w-100px">{{ n }}</th> -->
-                                            <th class=" min-w-100px">{{ t('form.name') }}</th>
-                                            <th class=" min-w-100px">{{ t('student.email') }}</th>
-                                            <th class=" min-w-100px">{{ t('student.phone') }}</th>
-                                            <th class=" min-w-100px">{{ t('student.nationality') }}</th>
-                                            <th class=" min-w-100px">{{ t('student.gender') }}</th>
-                                            <th class=" min-w-100px">{{ t('student.enrollment_status') }}</th>
-                                            <th class=" min-w-100px">{{ t('student.date_of_birth') }}</th>
-                                            <th class=" min-w-100px">{{ t('student.is_active') }}</th>
-                                            <th class=" min-w-100px">{{ t('student.country') }}</th>
-                                            <th class=" min-w-100px">{{ t('student.city') }}</th>
-                                            <th class=" min-w-100px">{{ t('student.street') }}</th>
-                                            <th class="text-end min-w-100px">Actions</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody class="fw-semibold text-gray-600">
-                                        <tr v-for="(item, index) in tbody" :key="index">
-                                            <td>
-                                                <div
-                                                    class="form-check form-check-sm form-check-custom form-check-solid">
-                                                    <input class="form-check-input" type="checkbox" value="1" />
-                                                </div>
-                                            </td>
-                                            <td class="">
-                                                <span class="fw-bold">{{ item.id }}</span>
-                                            </td>
-                                            <td class="">
-                                                <span class="fw-bold">{{ item.user.name }}</span>
-                                            </td>
-                                            <td class="">
-                                                <span class="fw-bold">{{ item.user.email ?? '-' }}</span>
-                                            </td>
-                                            <td class="">
-                                                <span class="fw-bold">{{ item.user.phone ?? '-' }}</span>
-                                            </td>
-                                            <td class="">
-                                                <span class="fw-bold">{{ item.nationality ?? '-' }}</span>
-                                            </td>
-                                            <td class="">
-                                                <span class="fw-bold">{{ item.user.gender }}</span>
-                                            </td>
-                                            <td class="">
-                                                <div
-                                                    :class="`badge badge-light-${item.enrollment_status === 'active' ? 'success' : 'danger'}`">
-                                                    <span class="fw-bold">{{ item.enrollment_status }}</span>
-                                                </div>
-                                            </td>
-                                            <td class="">
-                                                <span class="fw-bold">{{ item.user.date_of_birth }}</span>
-                                            </td>
-                                            <td class="">
-                                                <div
-                                                    :class="`badge badge-light-${item.user.is_active ? 'success' : 'danger'}`">
-                                                    <span class="fw-bold">{{ item.user.is_active ? 'Active' :
-                                                        'Inactive' }}</span>
-                                                </div>
-                                            </td>
-                                            <td class="">
-                                                <span class="fw-bold">{{ item.user.address.country }}</span>
-                                            </td>
-                                            <td class="">
-                                                <span class="fw-bold">{{ item.user.address.city }}</span>
-                                            </td>
-                                            <td class="">
-                                                <span class="fw-bold">{{ item.user.address.street }}</span>
-                                            </td>
-                                            <td class="text-end">
-                                                <div class="dropdown">
-                                                    <a href="#"
-                                                        class="btn btn-sm btn-light btn-flex btn-center btn-active-light-primary"
-                                                        data-bs-toggle="dropdown" aria-expanded="false"><i
-                                                            class="ki-solid ki-dots-horizontal fs-2x"></i>
-                                                    </a>
-                                                    <!--begin::Menu-->
-                                                    <div class=" dropdown-menu menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-125px py-4"
-                                                        data-kt-menu="true">
-                                                        <div class="menu-item px-3">
-                                                            <a href="#" @click="openEditModal(item)"
-                                                                class="menu-link px-3">{{ t('table.edit') }}</a>
-                                                        </div>
-                                                        <div v-if="route.name == 'student'" class="menu-item px-3">
-                                                            <a href="#" @click="deleteStudent(item.id)"
-                                                                class="menu-link px-3">{{ t('table.delete') }}</a>
-                                                        </div>
-                                                        <div v-if="route.name == 'student.onlyTrashed'"
-                                                            class="menu-item px-3">
-                                                            <a href="#" @click="restoreStudent(item.id)"
-                                                                class="menu-link px-3">Restore</a>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
+            <div class="card-header align-items-center py-5 gap-2 gap-md-5">
+                <div class="card-title">
+                    <div class="d-flex align-items-center position-relative my-1">
+                        <h1>{{ t('student.students') }}</h1>
                     </div>
                 </div>
+                <div class="card-toolbar flex-row-fluid justify-content-end gap-5">
+                    <!--begin::Add product-->
+                    <button @click="openAddModal" class="btn btn-primary">{{ t('table.add') }} {{
+                        t('student.student') }}</button>
+                    <router-link v-if="route.name == 'student'" :to="{ name: 'student.onlyTrashed' }"
+                        class="btn btn-primary">{{ t('student.students') }} {{ t('student.trashed')
+                        }}</router-link>
+                    <router-link v-if="route.name == 'student.onlyTrashed'" :to="{ name: 'student' }"
+                        class="btn btn-primary">{{ t('student.students') }} Active</router-link>
+                    <Filter @submit="fetchStudents" />
+                </div>
             </div>
+            <!-- Table -->
+            <div class="card-body pt-0 table-responsive">
+                <table class="table align-middle table-row-dashed fs-6 gy-5">
+                    <thead>
+                        <tr class="text-start text-gray-400 fw-bold fs-7 text-uppercase gs-0">
+                            <th class="w-10px pe-2">
+                                <div class="form-check form-check-sm form-check-custom form-check-solid me-3">
+                                    <input class="form-check-input" type="checkbox" data-kt-check="true"
+                                        data-kt-check-target="#kt_ecommerce_products_table .form-check-input"
+                                        value="1" />
+                                </div>
+                            </th>
+                            <th class="min-w-100px">{{ t('form.id') }}</th>
+                            <!-- <th v-for="n in thead" class=" min-w-100px">{{ n }}</th> -->
+                            <th class=" min-w-100px">{{ t('form.name') }}</th>
+                            <th class=" min-w-100px">{{ t('student.email') }}</th>
+                            <th class=" min-w-100px">{{ t('student.phone') }}</th>
+                            <th class=" min-w-100px">{{ t('student.nationality') }}</th>
+                            <th class=" min-w-100px">{{ t('student.gender') }}</th>
+                            <th class=" min-w-100px">{{ t('student.enrollment_status') }}</th>
+                            <th class=" min-w-100px">{{ t('student.date_of_birth') }}</th>
+                            <th class=" min-w-100px">{{ t('student.is_active') }}</th>
+                            <th class=" min-w-100px">{{ t('student.country') }}</th>
+                            <th class=" min-w-100px">{{ t('student.city') }}</th>
+                            <th class=" min-w-100px">{{ t('student.street') }}</th>
+                            <th class="text-end min-w-100px">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody class="fw-semibold text-gray-600">
+                        <tr v-for="(item, index) in tbody" :key="index">
+                            <td>
+                                <div class="form-check form-check-sm form-check-custom form-check-solid">
+                                    <input class="form-check-input" type="checkbox" value="1" />
+                                </div>
+                            </td>
+                            <td class="">
+                                <span class="fw-bold">{{ item.id }}</span>
+                            </td>
+                            <td class="">
+                                <span class="fw-bold">{{ item.user.name }}</span>
+                            </td>
+                            <td class="">
+                                <span class="fw-bold">{{ item.user.email ?? '-' }}</span>
+                            </td>
+                            <td class="">
+                                <span class="fw-bold">{{ item.user.phone ?? '-' }}</span>
+                            </td>
+                            <td class="">
+                                <span class="fw-bold">{{ item.nationality ?? '-' }}</span>
+                            </td>
+                            <td class="">
+                                <span class="fw-bold">{{ item.user.gender }}</span>
+                            </td>
+                            <td class="">
+                                <div
+                                    :class="`badge badge-light-${item.enrollment_status === 'active' ? 'success' : 'danger'}`">
+                                    <span class="fw-bold">{{ item.enrollment_status }}</span>
+                                </div>
+                            </td>
+                            <td class="">
+                                <span class="fw-bold">{{ item.user.date_of_birth }}</span>
+                            </td>
+                            <td class="">
+                                <div :class="`badge badge-light-${item.user.is_active ? 'success' : 'danger'}`">
+                                    <span class="fw-bold">{{ item.user.is_active ? 'Active' :
+                                        'Inactive' }}</span>
+                                </div>
+                            </td>
+                            <td class="">
+                                <span class="fw-bold">{{ item.user.address.country }}</span>
+                            </td>
+                            <td class="">
+                                <span class="fw-bold">{{ item.user.address.city }}</span>
+                            </td>
+                            <td class="">
+                                <span class="fw-bold">{{ item.user.address.street }}</span>
+                            </td>
+                            <td class="text-end">
+                                <div class="dropdown">
+                                    <a href="#"
+                                        class="btn btn-sm btn-light btn-flex btn-center btn-active-light-primary"
+                                        data-bs-toggle="dropdown" aria-expanded="false"><i
+                                            class="ki-solid ki-dots-horizontal fs-2x"></i>
+                                    </a>
+                                    <!--begin::Menu-->
+                                    <div class=" dropdown-menu menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-125px py-4"
+                                        data-kt-menu="true">
+                                        <div class="menu-item px-3">
+                                            <a href="#" @click="openEditModal(item)" class="menu-link px-3">{{
+                                                t('table.edit') }}</a>
+                                        </div>
+                                        <div v-if="route.name == 'student'" class="menu-item px-3">
+                                            <a href="#" @click="deleteStudent(item.id)" class="menu-link px-3">Delete</a>
+                                        </div>
+                                        <div v-if="route.name == 'student.onlyTrashed'" class="menu-item px-3">
+                                            <a href="#" @click="restoreStudent(item.id)"
+                                                class="menu-link px-3">Restore</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </template>
+        <template #modal>
             <Create v-model="showModal" :isEdit="isEdit" :studentData="selectedStudent" @submit="handleSave"
                 :errors="validationErrors" />
         </template>
-    </Master>
+    </Index>
 </template>
 
 <script setup>
-import Master from '@/components/MainContent.vue'
+import Index from '@/components/pages/Index'
 import Create from './Create.vue';
 import Filter from './Filter.vue';
 import api from '@/services/api'
-import Alert from '@/components/common/Alert.vue';
 import { RouterLink, useRouter, useRoute } from 'vue-router';
 import { ref, watch } from 'vue'
 import { useI18n } from 'vue-i18n';
